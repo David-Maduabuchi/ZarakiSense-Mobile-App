06b54a10a750de804d1b9ac22288fdf93a282ba4
David Maduabuchi <davidmaduabuchi62@gmail.com>
2025-06-15 23:47:56 +0100

    Second Tree

---
+++ ZarakiSense/app/auth/welcome.tsx
@@ -0,0 +1,57 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        {/* Login button */}
+        <View>
+          <TouchableOpacity style={styles.loginButton}>
+            <Typo fontWeight={500}>Sign In</Typo>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: spacingY._7,
+  },
+  welcomeImage: {
+    width: "100%",
+    height: verticalScale(300),
+    alignSelf: "center",
+    marginTop: verticalScale(100),
+  },
+  loginButton: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+  footer: {
+    backgroundColor: colors.neutral900,
+    alignItems: "center",
+    paddingTop: verticalScale(30),
+    paddingBottom: verticalScale(45),
+    gap: spacingY._20,
+    shadowColor: "white",
+    shadowOffset: { width: 0, height: -10 },
+    elevation: 10,
+    shadowRadius: 25,
+    shadowOpacity: 0.15,
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: spacingX._25,
+  },
+});
---
+++ ZarakiSense/constants/theme.ts
@@ -0,0 +1,70 @@
+import { scale, verticalScale } from "@/utils/styling";
+
+export const colors = {
+  primary: "#3a6e35",
+  primaryLight: "#9ea5e9",
+  primaryDark: "#063a91",
+  text: "#fff",
+  textLight: "#e5e5e5",
+  textLighter: "#d4d4d4",
+  white: "#fff",
+  black: "#000",
+  rose: "#fe4044",
+  green: "#16a34a",
+  neutral100: "#fafafa",
+  neutral120: "#f5f5f5",
+  neutral150: "#e5e5e5",
+  neutral200: "#d4d4d4",
+  neutral250: "#CCCCCC",
+  neutral300: "#a3a3a3",
+  neutral350: "#737373",
+  neutral400: "#525252",
+  neutral500: "#404040",
+  neutral800: "#262626",
+  neutral900: "#171717",
+};
+
+export const spacingX = {
+  _3: scale(3),
+  _5: scale(5),
+  _7: scale(7),
+  _10: scale(10),
+  _12: scale(12),
+  _15: scale(15),
+  _17: scale(17),
+  _20: scale(20),
+  _25: scale(25),
+  _30: scale(30),
+  _35: scale(35),
+  _40: scale(40),
+  _50: scale(50),
+  _60: scale(60),
+};
+
+export const spacingY = {
+  _3: verticalScale(3),
+  _5: verticalScale(5),
+  _7: verticalScale(7),
+  _10: verticalScale(10),
+  _12: verticalScale(12),
+  _15: verticalScale(15),
+  _17: verticalScale(17),
+  _20: verticalScale(20),
+  _25: verticalScale(25),
+  _30: verticalScale(30),
+  _35: verticalScale(35),
+  _40: verticalScale(40),
+  _50: verticalScale(50),
+  _60: verticalScale(60),
+};
+
+export const radius = {
+  _3: verticalScale(3),
+  _6: verticalScale(6),
+  _10: verticalScale(10),
+  _12: verticalScale(12),
+  _15: verticalScale(15),
+  _17: verticalScale(17),
+  _20: verticalScale(20),
+  _30: verticalScale(30),
+};
